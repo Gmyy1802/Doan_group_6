b44b7f6de64f7beba19e7d1c6275822657e8a770
Tuấn Minh <minhhtuann195@gmail.com>
2026-02-27 15:11:19 +0700

    Remove .vs and add gitignore

---
+++ QuanLiBanHang/HienThiSanPham/ChiTietSanPhamWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System.Windows;
+
+namespace HienThiSanPham
+{
+    public partial class ChiTietSanPhamWindow : Window
+    {
+        public ChiTietSanPhamWindow(SanPham sp)
+        {
+            InitializeComponent();
+
+            txtMa.Text = sp.MaSP;
+            txtTen.Text = sp.TenSP;
+            txtGia.Text = sp.DonGia.ToString();
+        }
+    }
+}
